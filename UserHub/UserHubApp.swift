454077ed7792c696cb94952d8cf319820f6e7820
haqim007 <khaqim41@yahoo.com>
2024-12-21 19:53:38 +0700

    Initial Commit

---
+++ UserHub/UserHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserHubApp.swift
+//  UserHub
+//
+//  Created by Haqim Macbook on 21/12/24.
+//
+
+import SwiftUI
+
+@main
+struct UserHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
